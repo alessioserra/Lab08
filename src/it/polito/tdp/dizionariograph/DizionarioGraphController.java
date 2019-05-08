d6eed5af702c287e988387e54932bb2bf01fed56
zierp <zierp@172.22.14.64>
2019-05-08 11:20:12 +0200

    interfaccia grafica

--- src/it/polito/tdp/dizionariograph/DizionarioGraphController.java
+++ src/it/polito/tdp/dizionariograph/DizionarioGraphController.java
@@ -1,5 +1,74 @@
 package it.polito.tdp.dizionariograph;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import it.polito.tdp.dizionariograph.model.Model;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class DizionarioGraphController {
 	
+	Model model = new Model();
+	
+	public void setModel(Model model) {
+		this.model=model;
+	}
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField txtLettere;
+
+    @FXML
+    private TextField txtCercare;
+
+    @FXML
+    private Button btnGeneraGrafo;
+
+    @FXML
+    private Button btnTrovaVicini;
+
+    @FXML
+    private Button btnGradoMax;
+
+    @FXML
+    private Button btnReset;
+
+    @FXML
+    void doGradoMax(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doGrafo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doReset(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doTrovaVicini(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert txtLettere != null : "fx:id=\"txtLettere\" was not injected: check your FXML file 'DizionarioGraph.fxml'.";
+        assert txtCercare != null : "fx:id=\"txtCercare\" was not injected: check your FXML file 'DizionarioGraph.fxml'.";
+        assert btnGeneraGrafo != null : "fx:id=\"btnGeneraGrafo\" was not injected: check your FXML file 'DizionarioGraph.fxml'.";
+        assert btnTrovaVicini != null : "fx:id=\"btnTrovaVicini\" was not injected: check your FXML file 'DizionarioGraph.fxml'.";
+        assert btnGradoMax != null : "fx:id=\"btnGradoMax\" was not injected: check your FXML file 'DizionarioGraph.fxml'.";
+        assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'DizionarioGraph.fxml'.";
+
+    }
 }
